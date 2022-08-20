7d902fbf39f969391add15b7f199b7cbb4397c8c
Grant Hill <granthill@live.com>
2022-08-20 08:30:31 -0400

    first commit.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            string reponse;
+
+            if (answer < 9)
+            {
+                reponse = answer + " less than nine.";
+            }
+            else
+            {
+                reponse = answer + "greater than or equal to nine.";
+            }
+            var response = (answer < 9) ? $"{answer} is less than nine." : $"{answer} is greater than or equal to nine.";
+            Console.WriteLine(response);
+        }
+    }
+}
